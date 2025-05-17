3e175c6698605b2e824a0b8d2ef3b466fa76cec0
kartik soni <162726029+Kartikeysoni1@users.noreply.github.com>
2025-05-17 12:59:45 +0530

    Update decToBin.cpp

--- Lecture006 Binary & Decimal Number System/decToBin.cpp
+++ Lecture006 Binary & Decimal Number System/decToBin.cpp
@@ -4,24 +4,35 @@ using namespace std;
 
 
 int main() {
-
     int n;
-    cin >> n;
+cout<<"enter the val :";
+cin >> n;
 
 
-    int ans  = 0;
-    int i = 0;
-    while(n != 0 ) {
+int ans  = 0;
+int i = 1;
+int place=1;
+while(n != 0 ) {
 
-        int bit  = n & 1;
+    int bit  = n & 1;
 
-        ans = (bit * pow(10, i) ) + ans;
+    ans=ans+bit*i;
+    i=i*10;
+    n=n>>1;
 
-        n = n >> 1;
-        i++;
+        
 
-    }
+}
 
-    cout<<" Answer is " << ans << endl;
+cout<<" Answer is " << ans << endl;
+
+
+    
+
+return 0;
+}
+
+     
+
     return 0;
 }
